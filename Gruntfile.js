53d95a1dd2de87b9204fa83011ce957c30c9d655
JoaquinDev <joaquin@isla.io>
2014-02-14 10:53:28 +0100

    Add Grunt usage
    Add Grunt usage
    
    Contact: joaquin@isla.io

---
+++ Gruntfile.js
@@ -0,0 +1,58 @@
+/* Source: http://gruntjs.com/sample-gruntfile  */
+module.exports = function(grunt){
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        //define a string to put between each file in the concatenated output
+        separator: ';'
+      },
+      dist: {
+        //the files to concatenate
+        src: ['src/**/*.js'],
+        //the location of the resulting JS file
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    }, 
+    uglify: {
+      options: {
+        //the banner is inserted at the top of the output
+        banner: '/*! <%= pkg.author %> <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    }, 
+    jshint: {
+      //define the files to lint
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'], 
+      //configure JSHint
+      options: {
+        //more options here if you want to override JSHint defaults
+        globals: {
+          jQuery: true, 
+          console: true,
+          module: true
+        }
+      }
+    }, 
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint', 'qunit']
+    }
+  });
+
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  //this would be run by typing "grunt test" on the command line
+  grunt.registerTask('test', ['jshint']);
+  //the default task can be run just by typing "grunt" on the command line
+  grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+
+};
--- README.md
+++ README.md
@@ -2,3 +2,7 @@ ponghtml5game-c-s
 =================
 
 This repo contains the code when transcoding/transcribing the code of this other awesome repository https://github.com/danielmahal/Pong
+
+Source: [danielmahal/Pong repository][url-source]
+
+[url-source]: https://github.com/danielmahal/Pong 
